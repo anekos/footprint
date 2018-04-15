a788d2888755da5a7411a931818010806f35cb71
anekos <anekos@snca.net>
2018-04-15 21:46:04 +0900

    Hash remembers selected tag and target

--- src/js/main_view.js
+++ src/js/main_view.js
@@ -1,6 +1,7 @@
 
 import Vue from 'vue'
 import Bootstrap from 'bootstrap'
+import JQuery from 'jquery'
 import Footprint from './footprint.js'
 import Util from './util.js'
 
@@ -10,11 +11,24 @@ import draggable from 'vuedraggable'
 async function main() {
   let targets = await Footprint.targets();
   let config = await Footprint.getConfig();
+  let realTags = Footprint.Helper.extractTags(targets, config.tagOrder);
 
   targets.forEach(target => {
     target.newTags = [].concat(target.tags);
   });
   targets.sort((a, b) => b.lastUpdatedAt - a.lastUpdatedAt);
+
+  let state = (() => {
+    let values = window.location.hash.replace(/^#/, '').split('-');
+    let tag = parseInt(values[0]) || 0;
+    let target = parseInt(values[1]);
+    if (isNaN(target))
+      target = undefined;
+    if (realTags.length + 2 <= tag)
+      tag = 0;
+    return {tag, target};
+  })();
+
 
   let app = new Vue({
     el: '#app',
@@ -22,8 +36,8 @@ async function main() {
       draggable,
     },
     data: {
-      targets: targets,
-      realTags: Footprint.Helper.extractTags(targets, config.tagOrder),
+      targets,
+      realTags: realTags,
       pseudoTags: [
         {name: 'NONE', isIn: tags => tags.length == 0},
         {name: 'ALL', isIn: _ => true},
@@ -31,6 +45,7 @@ async function main() {
       newTagName: '',
       tagNameToRemove: [],
       fileToImport: null,
+      state,
     },
     computed: {
       tags: function () {
@@ -88,9 +103,20 @@ async function main() {
       updateTagOrder: async function () {
         config.tagOrder = this.realTags.concat([]);
         await Footprint.updateConfig(config);
+      },
+      updateLocationHash: function (name, value) {
+        this.state[name] = value;
+        let hash = [this.state.tag, this.state.target];
+        window.location.hash = hash.join('-');
       }
     }),
   });
+
+  JQuery(Util.id('#tab', state.tag)).addClass('active');
+  JQuery(Util.id('#tab-link', state.tag)).addClass('active');
+  if (typeof state.target !== 'undefined') {
+    JQuery(Util.id('#target', state.tag, state.target)).addClass('show');
+  }
 
 }
 
